f7b5d5b79ac672be3262fa9f0992d69276cde971
Ben-Devops <bencohen646@gmail.com>
2022-11-25 14:06:45 +0200

    added css

--- devclub-bencohen/src/components/books.js
+++ devclub-bencohen/src/components/books.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { getDatagoogle, onChangeinput } from "../assets/constants";
+import { Grid } from "@material-ui/core";
+import { Divider } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles({
+  divider: {
+    width: "100%",
+    height: "0.1ch",
+    maxWidth: 1200,
+    backgroundColor: "theme.palette.background.paper",
+    margin: "11px 0px 11px 0px",
+  },
+  root: {
+    backgroundColor: "var(--main-color-regular)",
+    width: "50%",
+    borderRight: "1px solid var(--main-color-light)",
+    overflow: "hidden",
+  },
+});
 const Books = () => {
+  const classes = useStyles();
   const [input, setInput] = useState("");
   const [loadinginfo, setLoadingInfo] = useState(false);
   const [books, setBooks] = useState([]);
@@ -39,39 +58,53 @@ const Books = () => {
   }, [input]);
 
   return (
-    <div className="container">
-      <span>Search:</span>
-      <input value={input} name="query" type="text" onChange={onChangeinput} />
-
-      <button className="btn-1">search</button>
-
-      {books?.length > 0 && (
-        <div className="books">
-          <ul>
-            {books?.map((book, index) => (
-              <li key={index} onClick={showBookInfo}>
-                {book.volumeInfo.title}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-      {loadinginfo && (
-        <div className="details">
-          <span>Author: {specificBook.authors} </span>
-          <br></br>
-          <span>Publish year: {specificBook.publishedDate}</span>
-        </div>
-      )}
-      <button className="wishlist btn" onClick={handleWishList}>
-        add to wishList
-      </button>
-      {wishlist?.map((element, index) => (
-        <div key={index + "abc"} className="wishlist">
-          {element}
-        </div>
-      ))}
-    </div>
+    <>
+      <Grid item container className={classes.root}>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <div class="container">
+            <span>Search:</span>
+            <input
+              value={input}
+              name="query"
+              type="text"
+              onChange={onChangeinput}
+            />
+          </div>
+        </Grid>
+        <Divider className={classes.divider} />
+        {books?.length > 0 && (
+          <div className="books">
+            <ul>
+              {books?.map((book, index) => (
+                <li key={index} onClick={showBookInfo}>
+                  {book.volumeInfo.title}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <Divider className={classes.divider} />
+        {loadinginfo && (
+          <div className="details">
+            <span>Author: {specificBook.authors} </span>
+            <br></br>
+            <span>Publish year: {specificBook.publishedDate}</span>
+          </div>
+        )}
+        <Divider className={classes.divider} />
+        <button className="wishlist btn" onClick={handleWishList}>
+          add to wishList
+        </button>
+        <Divider className={classes.divider} />
+        {wishlist?.map((element, index) => (
+          <p>
+            <div key={index + "abc"} className="wishlist">
+              {element}
+            </div>
+          </p>
+        ))}
+      </Grid>
+    </>
   );
 };
 export default Books;
